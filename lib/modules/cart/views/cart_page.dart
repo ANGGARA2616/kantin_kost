5e4db6eb68415f08e34f7cabd80f2ea0e40b96ff
MuhammadAnggara <anggaclasher55@webmail.umm.ac.id>
2025-12-02 11:00:56 +0700

    perbaikan gambar di halaman keranjang

--- lib/modules/cart/views/cart_page.dart
+++ lib/modules/cart/views/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/cart_controller.dart';
+import '../../../core/constants/app_constants.dart'; // TAMBAHKAN IMPORT INI
 
 class CartPage extends GetView<CartController> {
   const CartPage({super.key});
@@ -18,8 +19,7 @@ class CartPage extends GetView<CartController> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.shopping_cart,
-                    size: 64, color: Colors.grey), // Iconsax.shopping_cart
+                Icon(Icons.shopping_cart, size: 64, color: Colors.grey),
                 SizedBox(height: 16),
                 Text(
                   'Keranjang kosong',
@@ -55,23 +55,24 @@ class CartPage extends GetView<CartController> {
         padding: const EdgeInsets.all(12),
         child: Row(
           children: [
-            // Product Image
+            // Product Image - PERBAIKI DI SINI
             Container(
               width: 60,
               height: 60,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
                 color: Colors.grey[200],
-                image: item.product.imageUrl != null
+                image: item.product.imagePath != null // GANTI: imagePath bukan imageUrl
                     ? DecorationImage(
-                        image: NetworkImage(item.product.imageUrl!),
+                        image: NetworkImage(
+                          AppConstants.getProductImageUrl(item.product.imagePath!), // PAKAI HELPER
+                        ),
                         fit: BoxFit.cover,
                       )
                     : null,
               ),
-              child: item.product.imageUrl == null
-                  ? const Icon(Icons.fork_left,
-                      color: Colors.grey) // Ganti dengan icon yang tersedia
+              child: item.product.imagePath == null // GANTI: imagePath bukan imageUrl
+                  ? const Icon(Icons.fastfood, color: Colors.grey) // Icon yang lebih sesuai
                   : null,
             ),
             const SizedBox(width: 12),
